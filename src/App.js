b32b5bf506ae66469d1f01a1ffbb4e711d2c6597
maximpower <maxiimruso7@gmail.com>
2021-10-17 23:51:14 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
+const P = styled.p`
+  font-size: 24px;
+  color:red;
+`
+const Content = styled.div`
+  padding: 20px 25px;
+`
+
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Content>
+      <P>Hola, soy un párrafo</P>
+    </Content>
   );
 }
 
